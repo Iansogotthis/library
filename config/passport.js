f9c4839b1dc5c8d2163176f153ce796d89641877
= <=>
2024-02-26 13:09:48 -0600

    oauth and landing page

--- config/passport.js
+++ config/passport.js
@@ -1,31 +1,56 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-//Require your User Model here!
+// File where we can configure our passport strategies (login in with google, login with facebook, etc.... are all strategies)
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const UserModel = require("../models/user");
 
-// configuring Passport!
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // a user has logged in via OAuth!
-    // refer to the lesson plan from earlier today in order to set this up
+// this function middleware will be called everytime a USER LOG's in
+passport.use(
+  new GoogleStrategy(
+    // Configuration object, we need to tell google its our app!
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK,
+    },
+    // The verify callback function (Gets called everytime a user logs in!)
+    async function (accessToken, refreshToken, profile, cb) {
+      //< - information we want is in the profile object
+      // a user has logged in with OAuth...
 
+      // Search the database for users profile
+      // if we find a user with a profile.id that matches a users googleId
+   
+      let user = await UserModel.findOne({googleId: profile.id});
+       if (user) return cb(null, user)
+      try {
+        user = await UserModel.create({
+          name: profile.displayName,
+          googleId: profile.id,
+          email: profile.emails[0].value,
+          avatar: profile.photos[0].value
+        })
+
+        cb(null, user) 
+      } catch (err) {
+        return cb(err);
+      }
+    }
+  )
+);
+
+
+passport.serializeUser(function(user, cb){ 
+  cb(null, user._id)
+});
+
+
+
+passport.deserializeUser(async function(userId, cb){
+	try {
+    const userDoc = await UserModel.findById(userId)
+    cb(null, userDoc); 
+
+  } catch(err){
+    cb(err)
   }
-));
-
-passport.serializeUser(function(user, cb) {
-  
-});
-
-passport.deserializeUser(function(id, cb) {
-
-  // Find your User, using your model, and then call cb(err, whateverYourUserIsCalled)
-  // When you call this done function passport assigns the user document to req.user, which will 
-  // be availible in every Single controller function, so you always know the logged in user
-
-});
-
-
-
+})
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,9 @@ const router = require('express').Router();
 const passport = require('passport');
 
 // The root route renders our only view
-router.get('/', function(req, res) {
-  //UPDATE THIS ==========================================================================
-  // Where do you want to go for the root route (this is localhost:3000)
-  // in the student demo this was res.redirect('/movies'), what do you want?
-  // This could be a landing page, or just redirect to your main resource page which you'll have an a tag that makes 
-  // a request to `/auth/google` route below
-  //===============================================================================================
-});
+router.get('/', function(req, res, next) {
+  res.render('index') 
+ });
 
 // Google OAuth login route
 router.get('/auth/google', passport.authenticate(
@@ -19,12 +14,13 @@ router.get('/auth/google', passport.authenticate(
 
 // Google OAuth callback route
 router.get('/oauth2callback', passport.authenticate(
-  'google',
-  {
-    successRedirect : '/', // UPDATE THIS, where do you want the client to go after you login 
-    failureRedirect : '/' //  UPDATE THIS, where do you want the client to go if login fails
-  }
-));
+	'google',
+	{
+	  successRedirect: '/', // what route do you want to redirect if the login was success
+	  failureRedirect: '/' // what route do you want to redirect to if the login failed, its your choice!
+	}
+  ))
+  
 
 // OAuth logout route
 router.get('/logout', function(req, res){
